e97fef3f22dd15bf2c636a6ca4a33e774eb2476c
Eloi Sanchez <eloisanchez16@gmail.com>
2021-02-26 10:28:35 +0100

    Canviat dt -> dt_sim en input, parameters i init.f90

--- parameters.f90
+++ parameters.f90
@@ -1,10 +1,10 @@
       module parameters
       implicit none
+
       integer :: N,D
-      real*8 :: dt 
+      real*8 :: dt_sim
       integer :: n_meas, n_conf, n_total
       real*8 :: rho, T_ref, L, rc, fact_rc
       real*8 :: sigma, epsilon
-
 
       end module parameters
