3d4ded254962eb07100f68147922a58825d6a2fa
a96lex <a96lex@gmail.com>
2021-02-28 20:58:21 +0100

    updated movie creation

--- molecule_plotter/main.py
+++ molecule_plotter/main.py
@@ -1,16 +1,22 @@
+import sys
 import os
 from ase import io
-import imageio
 
-atoms = io.read('../results/trajectory.xyz', index=":")
+if len(sys.argv) == 1:
+    filename = "trajectory.gif"
+elif sys.argv[1][-4:] != ".gif":
+    filename = "{}.gif".format(sys.argv[1])
+else:
+    filename = sys.argv[1]
 
-images=[]
-for i in range(1000):
-    filename="p{}.png".format(i)
-    io.write(filename,atoms[i])
-    images.append(imageio.imread(filename))
-    os.remove(filename)
+if not os.path.exists("./results/plots"):
+    os.makedirs("./results/plots")
 
-imageio.mimsave('movie.gif', images)
-
-
+try:
+    atoms = io.read("./results/trajectory.xyz", index=":")
+    io.write("./results/plots/{}".format(filename), atoms, interval=50)
+    print("{} was created".format(filename))
+except:
+    print(
+        "Structure trajectory could not be found. Possible reasons:\n - Did you try to plot it before creating it?\n - Did you move or rename the results/trajectory.xyz file?"
+    )
