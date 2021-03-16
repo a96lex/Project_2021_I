b8952d68450479d27b0d17a39cea3104c26bdb0c
Eloi Sanchez <eloisanchez16@gmail.com>
2021-03-16 14:01:27 +0100

    If somebody needs to do tests, the pos(D,N) array will be stored in the master core

--- parallel/main.f90
+++ parallel/main.f90
@@ -32,18 +32,18 @@ program main
     close(10)
 
     if(taskid==master) then
-    print*,"------------------------Parameters-------------------------------"
-    print"(A,X,I5,2X,A,X,I1)", "N=",N,"D=",D
-    print"(A,X,E14.7)","dt_sim=",dt_sim
-    print"(A,X,F4.2,2X,A,X,F5.2)","rho=",rho,"T=",T_ref
-    print"(A,X,F7.4,2X,A,X,F4.2)","eps=",epsilon,"sigma=",sigma,"rc=",rc
-    print"(A,X,I3,2X,I5,2X,I5)","n_meas,n_conf,n_total=",n_meas,n_conf,n_total
-    print*,"-----------------------------------------------------------------"
+        print*,"------------------------Parameters-------------------------------"
+        print"(A,X,I5,2X,A,X,I1)", "N=",N,"D=",D
+        print"(A,X,E14.7)","dt_sim=",dt_sim
+        print"(A,X,F4.2,2X,A,X,F5.2)","rho=",rho,"T=",T_ref
+        print"(A,X,F7.4,2X,A,X,F4.2)","eps=",epsilon,"sigma=",sigma,"rc=",rc
+        print"(A,X,I3,2X,I5,2X,I5)","n_meas,n_conf,n_total=",n_meas,n_conf,n_total
+        print*,"-----------------------------------------------------------------"
     end if
     call MPI_BARRIER(MPI_COMM_WORLD,ierror)
 
     ! Initialize positions and velocities
-    call init_sc(pos)
+    if (taskid == master) call init_sc(pos)
     ! call init_sc_paralel(pos)
 
     if(taskid==master) open(1,file="results/init_conf.xyz")
