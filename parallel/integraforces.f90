45fed623b3ef0655c795436c19720dc1e2da8502
a96lex <a96lex@gmail.com>
2021-03-16 18:48:43 +0100

    trying to parallelize

--- parallel/integraforces.f90
+++ parallel/integraforces.f90
@@ -79,5 +79,55 @@
                end if
          end subroutine compute_force_LJ
 
+         
+         subroutine andersen_therm(velocities,temperature)
+            !Author: Arnau Jurado
+            !     Applies the Andersen thermostat to a system of N particles. 
+            !           Input
+            !           -----
+            !                 v : real*8,dimension(D,N)
+            !                       Velocities of the system.
+            !                 T : real*8
+            !                       Target temperature of the thermostat.
+            !           Output
+            !           ------
+            !                 v : real*8,dimension(D,N)
+            !                       Velocities of the system after the thermostat application.
+               implicit none
+               include 'mpif.h'
+               real*8,intent(inout) :: velocities(D,N)
+               real*8,intent(in) :: temperature
+               real*8 :: std,nu,x1,x2,pi
+               integer :: i,j
 
+               if (taskid.eq.master) then
+                  do i=1,N
+                     do j=1,D
+                        call MPI_SEND(velocities(i,j),1,MPI_DOUBLE_PRECISION,mod(i,numproc),master,MPI_COMM_WORLD,ierror)
+                     enddo
+                  enddo
+               endif
+               
+               pi = 4d0*datan(1d0)
+               std = sqrt(temperature) !Standard deviation of the gaussian.
+               nu = 0.1
+
+
+               do i=1,numproc
+                  do i=1,N
+                        if (rand()<nu) then ! Check if a collision happens.
+                              do j=1,D
+                                    call MPI_RECEIVE(velocities(i,j), 1, MPI_DOUBLE_PRECISION, mod(i,numproc), 1, MPI_COMM_WORLD, stat, ierror)
+                                    x1 = rand()
+                                    x2 = rand()
+                                    velocities(i,j) = std*dsqrt(-2d0*(dlog(1d0-x1)))*dcos(2d0*pi*x2)
+                              enddo
+                        endif
+                  enddo
+               enddo
+
+               call MPI_BARRIER(MPI_COMM_WORLD,ierror)
+
+            end subroutine andersen_therm  
+                     
    end module integraforces
