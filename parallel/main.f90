ffbb74cbc03e8ac613e15f42463fdeb703a7d1cd
Jaume Ojer <jaume.ojer@gmail.com>
2021-03-31 11:36:08 +0200

    Actualitzacio main paralel

--- parallel/main.f90
+++ parallel/main.f90
@@ -10,8 +10,14 @@
       include 'mpif.h'
       character(len=50)   :: input_name
       real*8, allocatable :: pos(:,:), vel(:,:), fold(:,:), g_avg(:), g_squared_avg(:)
+      real*8, allocatable :: epotVECins(:), epotVEC(:), PVEC(:), ekinVEC(:), etotVEC(:), TinsVEC(:)
+      real*8, allocatable :: Xpos(:), Ypos(:), Zpos(:)
+      
       real*8              :: time,ekin,epot,Tins,P,etot
-      integer             :: i,ierror,Nshells,flag_g
+      real*8              :: epotAUX,epotMEAN,PMEAN,epotVAR,PVAR
+      real*8              :: ekinMEAN,ekinVAR,etotMEAN,etotVAR,TinsMEAN,TinsVAR
+      real*8              :: Xmean,Ymean,Zmean,Xvar,Yvar,Zvar
+      integer             :: i,ierror,Nshells,flag_g,k,cnt
       real*8              :: ti_global,tf_global,elapsed_time !AJ: collective timing of program.
       
       ! Init MPI
@@ -38,6 +44,15 @@
       allocate(pos(D,N))
       allocate(vel(D,N)) 
       allocate(fold(D,N))
+      allocate(epotVECins(n_meas))
+      allocate(PVEC(n_conf))
+      allocate(ekinVEC(n_conf))
+      allocate(epotVEC(n_conf))
+      allocate(etotVEC(n_conf))
+      allocate(TinsVEC(n_conf))
+      allocate(Xpos(N))
+      allocate(Ypos(N))
+      allocate(Zpos(N))
       
       if(taskid==master) then
             print*,"------------------------Parameters-------------------------------"
@@ -85,6 +100,9 @@
             open(unit=10,file="results/thermodynamics.dat")
             open(unit=11,file="results/trajectory.xyz")
             open(unit=12,file="results/radial_distribution.dat")
+            open(unit=13,file="results/mean_epot.dat")
+            open(unit=14,file="results/diffcoeff.dat")
+            open(unit=15,file="results/averages.dat")
       
             write(10,*)"#t,   K,   U,  E,  T,  v_tot,  Ptot"
       end if
@@ -97,18 +115,53 @@
             allocate(g_squared_avg(Nshells))
             g_avg = 0d0
             g_squared_avg = 0d0
+            
+            print*,"------Simulation Start------"
       endif
-  
-      !Start main simulation
+      
+      
+      k = 0
+      cnt = 0
+      epotAUX = 0.d0
+      
       call compute_force_LJ(pos,fold,epot,P)
       do i = 1,n_total
+      
             call verlet_v_step(pos,vel,fold,time,i,dt_sim,epot,P)
             call andersen_therm(vel,T_ref)
+            
+            ! Càlcul del coeficient de difusió per cada dimensió
+            Xpos(:) = pos(1,:)
+            Ypos(:) = pos(2,:)
+            Zpos(:) = pos(3,:)
+            call estad(N,Xpos,Xmean,Xvar)
+            call estad(N,Ypos,Ymean,Yvar)
+            call estad(N,Zpos,Zmean,Zvar)
+            if (taskid.eq.master) then
+                write(14,*) 2.d0*dble(i), Xvar*dble(N), Yvar*dble(N), Zvar*dble(N)
+            endif
+
+            k = k+1
+            epotVECins(k) = epot
       
             if(mod(i,n_meas) == 0) then ! AJ : measure every n_meas steps
+                  ! Average de epot cada n_meas. Ho escribim en un fitxer
+                  k = 0
+                  cnt = cnt+1
+                  call estad(n_meas,epotVECins,epotMEAN,epotVAR)
+                  if (taskid.eq.master) then
+                        epotAUX = (epotMEAN+epotAUX*dble(cnt-1))/dble(cnt)
+                        write(13,*) i, epotAUX
+                  endif
+                  
                   call energy_kin(vel,ekin,Tins)
                   if(taskid==master) then
                         write(10,*) time, ekin, epot, ekin+epot, Tins, dsqrt(sum(sum(vel,2)**2)), P+rho*Tins
+                        ekinVEC(cnt) = ekin
+                        epotVEC(cnt) = epot
+                        etotVEC(cnt) = ekin+epot
+                        TinsVEC(cnt) = Tins
+                        PVEC(cnt) = P+rho*Tins
                         call writeXyz(D,N,pos,11)
                   end if
                   call rad_dist_fun_pairs(pos,Nshells)
@@ -124,6 +177,15 @@
             endif
       enddo
       
+      if(taskid==master) then
+            write (*,*)
+            write (*,*) "----Simulation Completed----"
+            close(10)
+            close(11)
+            close(13)
+            close(14)
+      end if
+      
       ! Average de la g(r)
       if(taskid==master) then
             g_avg = g_avg/dble(n_conf)
@@ -132,13 +194,34 @@
             do i=1,Nshells
                   write(12,*) grid_shells*(i-1), grid_shells*(i-1)*sigma, g_avg(i), dsqrt(g_squared_avg(i) - g_avg(i)**2)
             enddo
+            close(12)
       endif
+
+      ! Averages finals
+      if (allocated(epotVECins)) deallocate(epotVECins)
+
+      call estad(n_conf,ekinVEC,ekinMEAN,ekinVAR)
+      call estad(n_conf,epotVEC,epotMEAN,epotVAR)
+      call estad(n_conf,etotVEC,etotMEAN,etotVAR)
+      call estad(n_conf,TinsVEC,TinsMEAN,TinsVAR)
+      call estad(n_conf,PVEC,PMEAN,PVAR)
       
-      if(taskid==master) then
-            write (*,*)
-            write (*,*) "----Simulation Completed----"
-      end if
-  
+      if (taskid.eq.master) then
+          write(15,*) "Sample mean and Variance"
+          write(15,*) "Kinetic Energy", ekinMEAN, ekinVAR
+          write(15,*) "Potential Energy", epotMEAN, epotVAR
+          write(15,*) "Total Energy", etotMEAN, etotVAR
+          write(15,*) "Instant Temperature", TinsMEAN, TinsVAR
+          write(15,*) "Pressure", PMEAN, PVAR
+          close(15)
+      endif
+
+      ! Binning de les energies cinètica i potencial
+      call binning(n_conf,ekinVEC,50,"results/ekinBIN.dat")
+      call binning(n_conf,epotVEC,50,"results/epotBIN.dat")
+      
+      ! Funció d'autocorrelació per l'energia total
+      call corrtime(n_conf,etotVEC,"results/correlation_energy.dat")
   
   
       if (allocated(pos)) deallocate(pos)
@@ -146,6 +229,14 @@
       if (allocated(fold)) deallocate (fold)
       if (allocated(aux_pos)) deallocate(aux_pos)
       if (allocated(aux_size)) deallocate(aux_size)
+      if (allocated(epotVEC)) deallocate(epotVEC)
+      if (allocated(PVEC)) deallocate(PVEC)
+      if (allocated(ekinVEC)) deallocate(ekinVEC)
+      if (allocated(etotVEC)) deallocate(etotVEC)
+      if (allocated(TinsVEC)) deallocate(TinsVEC)
+      if (allocated(Xpos)) deallocate(Xpos)
+      if (allocated(Ypos)) deallocate(Ypos)
+      if (allocated(Zpos)) deallocate(Zpos)
   
       tf_global = MPI_WTIME()
       call MPI_REDUCE(tf_global-ti_global,elapsed_time,1,MPI_DOUBLE_PRECISION,MPI_MAX,master,MPI_COMM_WORLD,ierror)
