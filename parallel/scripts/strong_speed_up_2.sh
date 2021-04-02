689d0a349cfbfa7bfb8e2360a41cba39a5742572
a96lex <a96lex@gmail.com>
2021-04-02 22:22:55 +0200

    numproc=2**i

--- parallel/scripts/strong_speed_up_2.sh
+++ parallel/scripts/strong_speed_up_2.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-echo -n  "Min number of processors: "
+echo -n  "Min number of processors (power): "
 read min_np
-echo -n "Max number of processors: "
+echo -n "Max number of processors (power): "
 read max_np
 
-for (( procs=min_np; procs<=max_np; ++procs))
+for (( i=min_np; i<=max_np; ++i))
 do
+    procs=$((2**i))
     echo $procs
     mkdir -p results/speedup/sim_${procs}
     sed "s/NUMPROCS/$procs/" sim_template.sub > ./results/speedup/sim_${procs}/script.sub
